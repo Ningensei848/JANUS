5aa18cc85ab5f7f9a68aa7721d691eae58f0f798
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2019-12-03 19:08:15 +0900

    MODIFY: change output filename

--- base/cron_job.sh
+++ base/cron_job.sh
@@ -3,7 +3,7 @@
 cd /server
 
 filename=$(date +%Y%m%d)
-stdout="/server/log/stdout.log"
+stdout="/server/log/${filename}_stdout.log"
 stderr="/server/log/${filename}_err.log"
 
 python /server/scrap.py >> $stdout 2>> $stderr
